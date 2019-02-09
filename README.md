eabcad3381d88bc4bd66522fe00c9583962e6541
scott <scott@spiffy-linux.scotttearle.com>
2019-02-09 11:33:02 -0800

    First Commit of quickbooks backup log check script

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # quickbooksscripts
 utility scripts for quickbooks
 
-verifyandsendiferror.ps1:
-Quickbooks does not have an email alert if the schedules backup fails.  This script checks the backup log and sends an email to stake holders.
+#verifyandsendiferror.ps1:
+Quickbooks does not have an email alert if the scheduled backup fails.  This can result in the data corruption not being caught until many days have passed, thereby creating a lot of manual work for the end user.  This script checks the backup log and sends an email to stake holders so they can revert to a very recent backup quickly, thereby saving them the headache of having to manually enter many days of work.
+
+This script assumes you have some familiarity with powershell and execution policies
+
+This is meant to be run on the database server, not workstation only systems.
+
+I use <> to denote places where you must change the script except for the emailto addresses which are
+a special case
+
+A basic log file of ok and failed backups with timestamps is created to help you find out when your issues
+started so you can recover quickly
+
+I recommend setting up a separate drive or network location for your backups.
+
+Once you have the script working you should create a new scheduled task to run this script shortly after your
+scheduled Quickbooks backup.  That way you will have fast notice so you can stop your users from working and return to a good working copy of Quickbooks. 
+
+I run my Quickbooks backups on an hourly basis so my users can recover from a data verification error quickly and have mininmal manual entries to catch up to.
+If you do this you may find that you will have to keep quite a number of copies so you can recover from at least 3 or more days previously.
+
---
+++ verifyandsendiferror.ps1
@@ -0,0 +1,68 @@
+﻿# Scott Tearle Feb 9, 2019
+# This script assumes you have some familiarity with powershell and execution policies
+# Script to check Quickbooks backup log and send emails if it fails.
+# This is meant to be run on the database server, not workstation only systems.
+#
+# I use <> to denote places where you must change the script except for the emailto addresses which are
+# a special case
+#
+# A basic log file of ok and failed backups with timestamps is created to help you find out when your issues
+# started so you can recover quickly
+#
+#
+# I recommend setting up a separate drive or network location for your backups.
+#
+# Copy the original QBBackup.log to a working location so as to not have file lock problems later
+# Change <Path to original QBBackup.log>
+# The original log file is usually located under
+# C:\Users\<the user you installed under>\AppData\Local\Intuit\QuickBooks\Log\QBBackup.log
+# Change <Path to working location> throughout the script i.e. E:\QuickbooksBU\QBBackup.log
+cp "<Path to original QBBackup.log>" "<Path to working location>"
+$data = Get-Content "<Path to working location>\QBBackup.log"
+$count = 0
+$lastentry = 0
+ForEach ($line IN $data){
+    $count += 1
+    if($line -eq "**********************************************"){
+        $lastentry = $count
+    }
+}
+$backupok = $true
+for($x=$lastentry; $x -lt $data.Length; $x++){
+    if ($data[$x] -like '*Failed*'){
+        $backupok = $false
+        # Write failure to log
+        &{
+          Write $(Get-Date)
+          Write "Quickbooks backup failure Sending Email"
+        } 3>&1 2>&1 >> <Path to working location>\verifyscript.log
+        #
+        # I use gmail to send my notifications.  You may have to change some of these settings
+        # to reflect your email server requirements
+        #
+        # Change this to a legitimate email address
+        $EmailFrom = "someuser@example.com"
+        # Powershell's emailer requires you to use an array of 
+        # email addresses in the format of  John <user@example.com>
+        # the <> must be included
+        # if you do not then the email will fail
+        $EmailTo = @("User1 <user1@gmail.com>", "User1 <user2@gmail.com>", "User1 <user3@gmail.com>") 
+        $Subject = "Notification of Quickbooks backup failure" 
+        $Body = $data[$x]
+        $SMTPServer = "smtp.gmail.com"
+        $SMTPClient = New-Object Net.Mail.SmtpClient($SMTPServer, 587) 
+        $SMTPClient.EnableSsl = $true 
+        $SMTPClient.Credentials = New-Object System.Net.NetworkCredential("user@example.com", "<password>"); 
+        $SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
+    }
+}
+if($backupok -eq $true){
+    &{
+        Write $(Get-Date)
+        Write "Backup OK"
+      } 3>&1 2>&1 >> <Path to working location>\verifyscript.log
+}
+
+
+
+ 
